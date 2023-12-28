6b987bc5f0b97f9e20d61389995ab93b188dfb37
Kristaps <kristaps1000@gmail.com>
2023-12-28 14:33:22 +0200

    highlight active nav element

--- app/@/components/navbar.tsx
+++ app/@/components/navbar.tsx
@@ -15,6 +15,7 @@ import {
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
 import { ThemeSelect } from "./theme-select";
+import { usePathname } from "next/navigation";
 
 import type { Role } from "@prisma/client";
 
@@ -38,6 +39,7 @@ const navLinks: navLinks = {
         {
           name: "Private customers",
           href: "/",
+          description: "Send parcels within Latvia and abroad",
         },
         {
           name: "Business customers",
@@ -68,8 +70,12 @@ const navLinks: navLinks = {
       href: "/",
     },
     {
-      name: "Orders",
-      href: "/",
+      name: "Send parcel",
+      href: "/parcel/send",
+    },
+    {
+      name: "Track parcel",
+      href: "/parcel/track",
     },
     {
       name: "Sign out",
@@ -85,60 +91,71 @@ const navLinks: navLinks = {
 const ListItem = React.forwardRef<
   React.ElementRef<"a">,
   React.ComponentPropsWithoutRef<"a">
->(({ className, title, children, ...props }, ref) => {
+>(({ className, title, children, href, ...props }, ref) => {
+  const pathname = usePathname();
+
   return (
     <li>
-      <NavigationMenuLink asChild>
-        <a
-          ref={ref}
-          className={cn(
-            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-            className,
-          )}
-          {...props}
-        >
-          <div className="text-sm font-medium leading-none">{title}</div>
-          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-            {children}
-          </p>
-        </a>
-      </NavigationMenuLink>
+      <Link href={href ?? ""} legacyBehavior passHref>
+        <NavigationMenuLink active={href === pathname} asChild>
+          <a
+            ref={ref}
+            className={cn(
+              "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground data-[active]:bg-accent/50",
+              className,
+            )}
+            {...props}
+          >
+            <div className="text-sm font-medium leading-none">{title}</div>
+            <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+              {children}
+            </p>
+          </a>
+        </NavigationMenuLink>
+      </Link>
     </li>
   );
 });
 ListItem.displayName = "ListItem";
 
-const navLinkMap = (item: NavItem) => (
-  <NavigationMenuItem key={item.name}>
-    {item.childs ? (
-      <>
-        <NavigationMenuTrigger>{item.name}</NavigationMenuTrigger>
-        <NavigationMenuContent>
-          <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-            {item.childs.map((child) => (
-              <ListItem key={child.name} title={child.name} href={child.href}>
-                {child.description}
-              </ListItem>
-            ))}
-          </ul>
-        </NavigationMenuContent>
-      </>
-    ) : (
-      <Link href={item.href} legacyBehavior passHref>
-        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-          {item.name}
-        </NavigationMenuLink>
-      </Link>
-    )}
-  </NavigationMenuItem>
-);
+const NavLinks = ({ navLinks }: { navLinks: NavItem[] }) => {
+  const pathname = usePathname();
+
+  return navLinks.map((item) => (
+    <NavigationMenuItem key={item.name}>
+      {item.childs ? (
+        <>
+          <NavigationMenuTrigger>{item.name}</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+              {item.childs.map((child) => (
+                <ListItem key={child.name} title={child.name} href={child.href}>
+                  {child.description}
+                </ListItem>
+              ))}
+            </ul>
+          </NavigationMenuContent>
+        </>
+      ) : (
+        <Link href={item.href} legacyBehavior passHref>
+          <NavigationMenuLink
+            active={item.href === pathname}
+            className={navigationMenuTriggerStyle()}
+          >
+            {item.name}
+          </NavigationMenuLink>
+        </Link>
+      )}
+    </NavigationMenuItem>
+  ));
+};
 
 export const Navbar = ({ role }: { role: Role | "default" }) => {
   return (
-    <div className="flex h-16 items-center justify-between px-4">
+    <div className="flex h-16 items-center justify-between">
       <NavigationMenu>
         <NavigationMenuList>
-          {navLinks[role].map(navLinkMap)}
+          <NavLinks navLinks={navLinks[role]} />
         </NavigationMenuList>
       </NavigationMenu>
 
