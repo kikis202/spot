80968271411ba8b953ba6f54ea84c41ff53a274d
Kristaps <kristaps1000@gmail.com>
2023-12-28 12:13:27 +0200

    prisma db seeder with 10 ParcelMachines

---
+++ app/prisma/seed.ts
@@ -0,0 +1,46 @@
+import { LockerSize, PrismaClient } from "@prisma/client";
+
+const getRandomLockerSize = () => {
+  const sizes = Object.values(LockerSize).filter((size) => size !== "CUSTOM");
+  const randomIndex = Math.floor(Math.random() * sizes.length);
+  const size = sizes[randomIndex];
+  if (!size) throw new Error("No locker size found");
+  return size;
+};
+
+const prisma = new PrismaClient();
+const main = async () => {
+  // Create 10 parcel machines with 10 lockers each
+  for (let i = 0; i < 10; i++) {
+    await prisma.parcelMachine.upsert({
+      where: { name: `Parcel Machine ${i}` },
+      update: {},
+      create: {
+        name: `Parcel Machine ${i}`,
+        address: {
+          create: {
+            street: `Street ${i}`,
+            city: `City ${i}`,
+            postalCode: `Postal Code ${i}`,
+            country: "Latvia",
+          },
+        },
+        lockers: {
+          create: Array.from({ length: 10 }, () => ({
+            size: getRandomLockerSize(),
+          })),
+        },
+      },
+    });
+  }
+};
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
