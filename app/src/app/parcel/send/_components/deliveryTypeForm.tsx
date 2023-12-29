591fecf32478e8c62802c9b206c9cea99dfaf3b8
Kristaps <kristaps1000@gmail.com>
2023-12-29 17:57:51 +0200

    Delivery type form with values stored in session storage and validation

--- app/src/app/parcel/send/_components/deliveryTypeForm.tsx
+++ app/src/app/parcel/send/_components/deliveryTypeForm.tsx
@@ -15,116 +15,312 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Textarea } from "@/components/ui/textarea";
+import { H2 } from "@/components/ui/typohraphy";
+import { Separator } from "@/components/ui/separator";
+import { Input } from "@/components/ui/input";
+import {
+  getSessionStorageValue,
+  setSessionStorageValue,
+} from "@/lib/clientUtils";
+import { useEffect, useMemo } from "react";
 
 const sendingOptions = ["parcelMachine", "courier"] as const;
 
-const deliverySchema = z.object({
-  weight: z.number().min(30).max(100).default(30),
-  size: z.nativeEnum(ParcelSize),
-  notes: z.string().optional(),
-  origin: z.enum(sendingOptions),
-  destination: z.enum(sendingOptions),
-});
+const deliverySchema = z
+  .object({
+    weight: z.number().min(1).max(100).optional(),
+    width: z.number().min(1).max(100).optional(),
+    height: z.number().min(1).max(100).optional(),
+    length: z.number().min(1).max(100).optional(),
+    size: z.nativeEnum(ParcelSize),
+    notes: z.string().optional(),
+    origin: z.enum(sendingOptions),
+    destination: z.enum(sendingOptions),
+    dimensions: z.string().optional(),
+  })
+  .refine(
+    ({ size, origin, destination }) =>
+      size !== ParcelSize.CUSTOM ||
+      (origin === "courier" && destination === "courier"),
+    {
+      message: "You can only send custom parcels via courier",
+      path: ["size"],
+    },
+  )
+  .refine(
+    ({ size, width }) => size !== ParcelSize.CUSTOM || width !== undefined,
+    {
+      message: "You must specify custom parcel dimensions",
+      path: ["width"],
+    },
+  )
+  .refine(
+    ({ size, height }) => size !== ParcelSize.CUSTOM || height !== undefined,
+    {
+      message: "You must specify custom parcel dimensions",
+      path: ["height"],
+    },
+  )
+  .refine(
+    ({ size, length }) => size !== ParcelSize.CUSTOM || length !== undefined,
+    {
+      message: "You must specify custom parcel dimensions",
+      path: ["length"],
+    },
+  )
+  .refine(
+    ({ size, weight }) => size !== ParcelSize.CUSTOM || weight !== undefined,
+    {
+      message: "You must specify custom parcel dimensions",
+      path: ["weight"],
+    },
+  );
+
+const CustomParcelFields = ({
+  form,
+}: {
+  form: ReturnType<typeof useForm<z.infer<typeof deliverySchema>>>;
+}) => {
+  return (
+    <div className="space-y-4">
+      <div className="flex items-center gap-6">
+        <FormField
+          control={form.control}
+          name="width"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Width</FormLabel>
+              <FormControl>
+                <Input
+                  {...field}
+                  type="number"
+                  className="w-20"
+                  placeholder="cm"
+                  onChange={(e) => {
+                    field.onChange(parseFloat(e.target.value));
+                  }}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="height"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Height</FormLabel>
+              <FormControl>
+                <Input
+                  {...field}
+                  type="number"
+                  className="w-20"
+                  placeholder="cm"
+                  onChange={(e) => {
+                    field.onChange(parseFloat(e.target.value));
+                  }}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="length"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Length</FormLabel>
+              <FormControl>
+                <Input
+                  {...field}
+                  type="number"
+                  className="w-20"
+                  placeholder="cm"
+                  onChange={(e) => {
+                    field.onChange(parseFloat(e.target.value));
+                  }}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+      </div>
+      <FormField
+        control={form.control}
+        name="weight"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Weight</FormLabel>
+            <FormControl>
+              <Input
+                {...field}
+                type="number"
+                className="w-20"
+                placeholder="cm"
+                onChange={(e) => {
+                  field.onChange(parseFloat(e.target.value));
+                }}
+              />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+    </div>
+  );
+};
 
 const DeliveryType = ({ nextStep }: { nextStep: () => void }) => {
+  const data = useMemo(() => getSessionStorageValue("deliveryType", {}), []);
+
   const form = useForm<z.infer<typeof deliverySchema>>({
     resolver: zodResolver(deliverySchema),
     defaultValues: {
       size: ParcelSize.SMALL,
-      weight: 30,
       origin: "parcelMachine",
       destination: "parcelMachine",
+      weight: 30,
+      width: 10,
+      height: 10,
+      length: 10,
     },
   });
 
+  useEffect(() => {
+    if (Object.keys(data).length === 0) return;
+    try {
+      form.reset(deliverySchema.parse(data));
+    } catch (e) {
+      console.log(e);
+    }
+  }, [data, form]);
+
+  const selectedSize = form.watch("size");
+
   const onSubmit = (data: z.infer<typeof deliverySchema>) => {
-    console.log(data);
+    setSessionStorageValue("deliveryType", data);
     nextStep();
   };
 
   return (
     <div>
+      <H2>Information about delivery</H2>
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)}>
-          <FormField
-            control={form.control}
-            name="size"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Parcel size</FormLabel>
-                <FormControl>
-                  <ToggleGroup
-                    type="single"
-                    className="justify-start"
-                    onValueChange={field.onChange}
-                    {...field}
-                  >
-                    <ToggleGroupItem value={ParcelSize.SMALL}>
-                      {ParcelSize.SMALL}
-                    </ToggleGroupItem>
-                    <ToggleGroupItem value={ParcelSize.MEDIUM}>
-                      {ParcelSize.MEDIUM}
-                    </ToggleGroupItem>
-                    <ToggleGroupItem value={ParcelSize.LARGE}>
-                      {ParcelSize.LARGE}
-                    </ToggleGroupItem>
-                    <ToggleGroupItem value={ParcelSize.XLARGE}>
-                      {ParcelSize.XLARGE}
-                    </ToggleGroupItem>
-                    <ToggleGroupItem value={ParcelSize.CUSTOM}>
-                      {ParcelSize.CUSTOM}
-                    </ToggleGroupItem>
-                  </ToggleGroup>
-                </FormControl>
-              </FormItem>
+          <div className="flex items-center space-x-6">
+            <div className="my-4 w-fit">
+              <FormField
+                control={form.control}
+                name="size"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Parcel size</FormLabel>
+                    <FormControl>
+                      <ToggleGroup
+                        type="single"
+                        className="justify-start"
+                        onValueChange={(event) => {
+                          field.onChange(event);
+                          if (event === ParcelSize.CUSTOM) {
+                            form.setValue("origin", "courier");
+                            form.setValue("destination", "courier");
+                          }
+                        }}
+                        {...field}
+                      >
+                        <ToggleGroupItem value={ParcelSize.SMALL}>
+                          {ParcelSize.SMALL}
+                        </ToggleGroupItem>
+                        <ToggleGroupItem value={ParcelSize.MEDIUM}>
+                          {ParcelSize.MEDIUM}
+                        </ToggleGroupItem>
+                        <ToggleGroupItem value={ParcelSize.LARGE}>
+                          {ParcelSize.LARGE}
+                        </ToggleGroupItem>
+                        <ToggleGroupItem value={ParcelSize.XLARGE}>
+                          {ParcelSize.XLARGE}
+                        </ToggleGroupItem>
+                        <ToggleGroupItem value={ParcelSize.CUSTOM}>
+                          {ParcelSize.CUSTOM}
+                        </ToggleGroupItem>
+                      </ToggleGroup>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Separator className="my-2" />
+              <div className="flex items-center gap-6">
+                <FormField
+                  control={form.control}
+                  name="origin"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Send from</FormLabel>
+                      <FormControl>
+                        <ToggleGroup
+                          type="single"
+                          className="justify-start"
+                          onValueChange={field.onChange}
+                          {...field}
+                        >
+                          {sendingOptions.map((option) => (
+                            <ToggleGroupItem
+                              disabled={
+                                selectedSize === ParcelSize.CUSTOM &&
+                                option === "parcelMachine"
+                              }
+                              key={option}
+                              value={option}
+                            >
+                              {option}
+                            </ToggleGroupItem>
+                          ))}
+                        </ToggleGroup>
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+                <Separator className="h-20" orientation="vertical" />
+                <FormField
+                  control={form.control}
+                  name="destination"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Send to</FormLabel>
+                      <FormControl>
+                        <ToggleGroup
+                          type="single"
+                          className="justify-start"
+                          onValueChange={field.onChange}
+                          {...field}
+                        >
+                          {sendingOptions.map((option) => (
+                            <ToggleGroupItem
+                              disabled={
+                                selectedSize === ParcelSize.CUSTOM &&
+                                option === "parcelMachine"
+                              }
+                              key={option}
+                              value={option}
+                            >
+                              {option}
+                            </ToggleGroupItem>
+                          ))}
+                        </ToggleGroup>
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </div>
+            {selectedSize === ParcelSize.CUSTOM && (
+              <Separator className="mx-2 h-32" orientation="vertical" />
             )}
-          />
-          <FormField
-            control={form.control}
-            name="origin"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Send from</FormLabel>
-                <FormControl>
-                  <ToggleGroup
-                    type="single"
-                    className="justify-start"
-                    onValueChange={field.onChange}
-                    {...field}
-                  >
-                    {sendingOptions.map((option) => (
-                      <ToggleGroupItem key={option} value={option}>
-                        {option}
-                      </ToggleGroupItem>
-                    ))}
-                  </ToggleGroup>
-                </FormControl>
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="destination"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Send to</FormLabel>
-                <FormControl>
-                  <ToggleGroup
-                    type="single"
-                    className="justify-start"
-                    onValueChange={field.onChange}
-                    {...field}
-                  >
-                    {sendingOptions.map((option) => (
-                      <ToggleGroupItem key={option} value={option}>
-                        {option}
-                      </ToggleGroupItem>
-                    ))}
-                  </ToggleGroup>
-                </FormControl>
-              </FormItem>
-            )}
-          />
+            <div className={selectedSize !== ParcelSize.CUSTOM ? "hidden" : ""}>
+              <CustomParcelFields form={form} />
+            </div>
+          </div>
           <FormField
             control={form.control}
             name="notes"
@@ -134,7 +330,7 @@ const DeliveryType = ({ nextStep }: { nextStep: () => void }) => {
                 <FormControl>
                   <Textarea
                     placeholder="Type your order notes here"
-                    className="resize-none"
+                    className="max-w-xl resize-none "
                     {...field}
                   />
                 </FormControl>
@@ -142,7 +338,9 @@ const DeliveryType = ({ nextStep }: { nextStep: () => void }) => {
               </FormItem>
             )}
           />
-          <Button type="submit">Submit</Button>
+          <Button className="mt-10" type="submit">
+            Submit
+          </Button>
         </form>
       </Form>
     </div>
