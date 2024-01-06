8c6f31cd9adbf25e416e918aa7a633f8229b3062
Kristaps <kristaps1000@gmail.com>
2024-01-06 18:21:05 +0200

    added footer to main layout

--- app/@/components/app-layout.tsx
+++ app/@/components/app-layout.tsx
@@ -2,17 +2,38 @@ import { getServerAuthSession } from "~/server/auth";
 
 import { type PropsWithChildren } from "react";
 import { Navbar } from "./navbar";
+import Link from "next/link";
+
+const Footer = () => (
+  <footer className="flex items-center justify-center bg-muted p-6">
+    <nav className="space-x-4">
+      <Link className="text-sm font-medium hover:underline" href="/tos">
+        Terms of Service
+      </Link>
+      <Link className="text-sm font-medium hover:underline" href="/privacy">
+        Privacy Policy
+      </Link>
+      <Link className="text-sm font-medium hover:underline" href="/faq">
+        FAQ
+      </Link>
+    </nav>
+    <div />
+  </footer>
+);
 
 export const MainLayout = async ({ children }: PropsWithChildren) => {
   const session = await getServerAuthSession();
   const role = session?.user?.role ?? "default";
 
   return (
-    <main>
-      <div className="mx-auto flex min-h-screen max-w-screen-2xl flex-col px-4">
-        <Navbar role={role} />
-        {children}
-      </div>
-    </main>
+    <>
+      <main>
+        <div className="mx-auto flex min-h-screen max-w-screen-2xl flex-col px-4">
+          <Navbar role={role} />
+          {children}
+        </div>
+      </main>
+      <Footer />
+    </>
   );
 };
